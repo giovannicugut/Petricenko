17b2083fbdf6fa31c5803169303bfeedc7d09930
Giovanni <giovannicugut@gmail.co>
2025-01-23 16:07:33 +0100

    inizio 0

---
+++ 2-switch-Case.js
@@ -0,0 +1,18 @@
+
+//   in qualchemodo Assomiglia al IF/ELSE ,da le condizione
+
+const nu =45;
+
+switch(nu){   //cerca il nr. richesto  tra le case : e ritorna la risposta
+    case 45:
+        console.log("prova piu un nr. piu grande")
+    break;
+    case 55:
+        console.log("tropo alto")
+    break;
+    case 50:
+        console.log("BINGOO")
+    break;
+    default:
+        console.log("error")
+}
