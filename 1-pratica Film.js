8d6b7d9e4aaec4333a8aec52df7b50eb58444e37
Giovanni <giovannicugut@gmail.co>
2025-01-23 11:50:10 +0100

    inizio

---
+++ 1-pratica Film.js
@@ -0,0 +1,19 @@
+const numberOfFilmS = +prompt("Quanti film hai visto")
+
+const personalMovieDB = {
+        count: numberOfFilmS,
+        movies: {},
+        actors: {},
+        gener: [],
+        privat: false,
+            }
+
+const dom1 = prompt("Scrive l'ultimom film che hai visto"),
+      dom2 = prompt("Da un voto al film "),
+      dom3 = prompt("Scrive film che ti ha piacioto particolareamente"),      
+      dom4 = prompt("Da un voto a questo film")
+     
+personalMovieDB.movies[dom1] = +dom2;
+personalMovieDB.movies[dom3] = +dom4;
+
+console.log(personalMovieDB)
