f54cb094c94cf27443327b2292cf71ce17d67550
Giovanni <giovannicugut@gmail.co>
2025-01-29 10:41:11 +0100

    aggiornato  fine

--- 1-pratica 1.js
+++ 1-pratica 1.js
@@ -1,6 +1,7 @@
 
 //  L'utente deve agg 2 film e voto,(aggiun su un array)
-/*
+
+
 const numberOfFilmS = +prompt("Quanti film hai visto")
 
 const personalMovieDB = {
@@ -10,14 +11,33 @@ const personalMovieDB = {
         gener: [],
         privat: false,
             }
-
-const dom1 = prompt("Scrive l'ultimom film che hai visto"),
-      dom2 = prompt("Da un voto al film "),
-      dom3 = prompt("Scrive film che ti ha piacioto particolareamente"),      
-      dom4 = prompt("Da un voto a questo film")
      
-personalMovieDB.movies[dom1] = +dom2;
-personalMovieDB.movies[dom3] = +dom4;
+
+for ( let i = 0; i < 2; i++){
+    const a = prompt("Scrive un film che hai visto"),
+          b = prompt("Da un voto al film ")
+
+    if(a != null && b !=null && a != "" && b!= "" && a != a.length < 20){
+        personalMovieDB.movies[a]= b
+        console.log("tutto OK")
+    } else{
+        alert("ERROR")
+        i--;
+    }
+}
 
 console.log(personalMovieDB)
-*/
+
+
+
+if(numberOfFilmS < 10){
+    console.log("NON sei un appasionato dei Film")
+}else if(numberOfFilmS >= 10 && numberOfFilmS < 20){
+    console.log("Guardi Film al livelo Normale")
+}else if( numberOfFilmS >= 20 ){
+    console.log(" sei un appasionato dei FILM")
+}
+else if(numberOfFilmS != "number"){
+    console.log(" Error, non e stato selezionato niete")
+}
+
