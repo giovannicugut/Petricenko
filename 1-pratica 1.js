f2ac067b2bee8dc11cd30c90d7bb1ba6a280b645
Giovanni <giovannicugut@gmail.co>
2025-02-13 10:43:43 +0100

    ho separato tutti function in modo di essere piu chiaro

--- 1-pratica 1.js
+++ 1-pratica 1.js
@@ -1,7 +1,7 @@
 
 //  L'utente deve agg 2 film e voto,(aggiun su un array)
 
-
+//   1)---------- functia  quanti film hai visto ----------------------------------------------------------------------
 let  numberOfFilmS;
 
 function start (){
@@ -11,8 +11,10 @@ function start (){
         numberOfFilmS = +prompt("Quanti film hai visto")
     }
 }
-//  start()
+  start()
 
+
+ //   2)------------ DB  ------------------------------------------------------------------------------------------------
 const personalMovieDB = {
         count: numberOfFilmS,
         movies: {},
@@ -20,7 +22,9 @@ const personalMovieDB = {
         gener: [],
         privat: false,
             }
-     
+
+
+  //   3) ------------- Scrivere film che hai visto di 2 volte--------------------------------------------------------------------------
 function rememberMyFilms(){      // nome del film visto e voto
 for ( let i = 0; i < 2; i++){
     const a = prompt("Scrive un film che hai visto").trim() ,  // .trim cancella spazi vouti(qui sara la risposta dal utente)
@@ -35,10 +39,13 @@ for ( let i = 0; i < 2; i++){
     }
 }
 }
-  // rememberMyFilms()
-// console.log(personalMovieDB)
+  //7 rememberMyFilms()
+ console.log(personalMovieDB)
 
 
+
+
+//  4)  ------------ Sei un appasionato dei film?-------------------------------------------------------------------------------------
 function detectPersonalLevel(){   // da una clasifica dipende da quantita di fil visti
 if(numberOfFilmS < 10){
     console.log("NON sei un appasionato dei Film")
@@ -51,8 +58,11 @@ else if(numberOfFilmS != "number"){
     console.log(" Error, non e stato selezionato niete")
 }
 }
-//  detectPersonalLevel()
+ // detectPersonalLevel()
 
+
+
+ //   5) ---- se e privato o publico-----------------------------------------------------------------------------------------------
 function showMyDB (para){
     if(para){
         console.log( " Non e privato")
@@ -61,10 +71,16 @@ function showMyDB (para){
 //  showMyDB(personalMovieDB.privat)
 
 
+
+//  6)  ------------ Inserire genere film 2 volte-------------------------------------------------------------------------------------
 function writeYourGenres (){
-    for (let i = 1; i <=3; i++){
+    for (let i = 1; i <=2; i++){
         const dom = prompt(`inserire il genere che ti piace sul posto ${i}`)
         personalMovieDB.gener[i-1] = dom
     }
 }
-  //writeYourGenres()
+ // writeYourGenres()
+  console.log(personalMovieDB.gener)
+
+
+ 
