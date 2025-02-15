d433a8f55856eec7f2668e96ac11e4f423f19fea
Giovanni <giovannicugut@gmail.co>
2025-02-15 19:18:03 +0100

    aggiunto esercizi arr

--- 1-pratica 1.js
+++ 1-pratica 1.js
@@ -1,86 +1,84 @@
 
 //  L'utente deve agg 2 film e voto,(aggiun su un array)
 
-//   1)---------- functia  quanti film hai visto ----------------------------------------------------------------------
-let  numberOfFilmS;
-
-function start (){
-    numberOfFilmS = +prompt("Quanti film hai visto")
-
-    while(numberOfFilmS =="" || numberOfFilmS == null || isNaN(numberOfFilmS)) {  // se avra quest consiz, rifare la domanda
-        numberOfFilmS = +prompt("Quanti film hai visto")
-    }
-}
-  start()
-
-
- //   2)------------ DB  ------------------------------------------------------------------------------------------------
 const personalMovieDB = {
-        count: numberOfFilmS,
+        count: 0,
         movies: {},
         actors: {},
         gener: [],
         privat: false,
-            }
+            
+        start: function(){  //functia  quanti film hai visto
+                personalMovieDB.count = +prompt("Quanti film hai visto")
+            
+                while(personalMovieDB.count =="" || personalMovieDB.count == null || isNaN(numberOfpersonalMovieDB.countFilmS)) {  // se avra quest consiz, rifare la domanda
+                    personalMovieDB.count = +prompt("Quanti film hai visto")
+                }
+            },
+        rememberMyFilms: function (){      // nome del film visto e voto
+            for ( let i = 0; i < 2; i++){
+                const a = prompt("Scrive un film che hai visto").trim() ,  // .trim cancella spazi vouti(qui sara la risposta dal utente)
+                      b = prompt("Da un voto al film ")
+                
+                    if(a != null && b !=null && a != "" && b!= "" && a != a.length < 20){
+                        personalMovieDB.movies[a]= b
+                        console.log("tutto OK")
+                    } else{
+                        alert("ERROR")
+                        i--;
+                    }
+                }
+                } ,
+        detectPersonalLevel: function(){   // da una clasifica dipende da quantita di film visti
+                if(numberOfFilmS < 10){
+                    console.log("NON sei un appasionato dei Film")
+                }else if(numberOfFilmS >= 10 && numberOfFilmS < 20){
+                    console.log("Guardi Film al livelo Normale")
+                }else if( numberOfFilmS >= 20 ){
+                      console.log(" sei un appasionato dei FILM")
+                   }
+                else if(numberOfFilmS != "number"){
+                        console.log(" Error, non e stato selezionato niete")
+                    }
+                 },
+        showMyDB: function (hidden){   // Contolla se e PRIVATO
+                    if(!hidden){
+                        console.log( " Non e privato")
+                    }
+                },
+        toggleVisibleMyDB: function(){
+                 if(personalMovieDB.privat){
+                        personalMovieDB.privat= false
+                }else {
+                        personalMovieDB.privat= true
+                    }
+                },
+        writeYourGenres: function (){    // inserire GENERE FILM
+            for (let i = 1; i <=2; i++){
+                const domanda = prompt(`inserire il genere che ti piace sul posto ${i}`)
+            
+                if(domanda == "" || domanda == null){
+                    console.log("errore di inserimento dati.  RIPROVARE")
+                    i--
+                }else{
+                    personalMovieDB.gener[i-1] = domanda
+                }
+            
+                personalMovieDB.gener.forEach((index,i)=>{
+                    console.log(`Il tuo gerenere sul Nr. ${i+1} e ${ index}`)
+                })
+            
+                }
+            
+                },
+       
+ }       
 
 
-  //   3) ------------- Scrivere film che hai visto di 2 volte--------------------------------------------------------------------------
-function rememberMyFilms(){      // nome del film visto e voto
-for ( let i = 0; i < 2; i++){
-    const a = prompt("Scrive un film che hai visto").trim() ,  // .trim cancella spazi vouti(qui sara la risposta dal utente)
-          b = prompt("Da un voto al film ")
-
-    if(a != null && b !=null && a != "" && b!= "" && a != a.length < 20){
-        personalMovieDB.movies[a]= b
-        console.log("tutto OK")
-    } else{
-        alert("ERROR")
-        i--;
-    }
-}
-}
-  //7 rememberMyFilms()
- console.log(personalMovieDB)
 
 
-
-
-//  4)  ------------ Sei un appasionato dei film?-------------------------------------------------------------------------------------
-function detectPersonalLevel(){   // da una clasifica dipende da quantita di fil visti
-if(numberOfFilmS < 10){
-    console.log("NON sei un appasionato dei Film")
-}else if(numberOfFilmS >= 10 && numberOfFilmS < 20){
-    console.log("Guardi Film al livelo Normale")
-}else if( numberOfFilmS >= 20 ){
-    console.log(" sei un appasionato dei FILM")
-}
-else if(numberOfFilmS != "number"){
-    console.log(" Error, non e stato selezionato niete")
-}
-}
- // detectPersonalLevel()
-
-
-
- //   5) ---- se e privato o publico-----------------------------------------------------------------------------------------------
-function showMyDB (para){
-    if(para){
-        console.log( " Non e privato")
-    }
-}
-//  showMyDB(personalMovieDB.privat)
-
-
-
-//  6)  ------------ Inserire genere film 2 volte-------------------------------------------------------------------------------------
-function writeYourGenres (){
-    for (let i = 1; i <=2; i++){
-        const dom = prompt(`inserire il genere che ti piace sul posto ${i}`)
-        personalMovieDB.gener[i-1] = dom
-    }
-}
- // writeYourGenres()
-  console.log(personalMovieDB.gener)
+ //---------------------------------
+ 
 
 
  
---
+++ 13- esercizi calcol arr-funct.js
@@ -0,0 +1,128 @@
+/*
+У вас есть небольшой кусочек данных о торговом центре, 
+которые записаны в объекте shoppingMallData. 
+Они содержат массив с данными о магазинах, где указана длина
+ и ширина помещения; высоту помещения; стоимость отопления 
+ за 1 кубический метр и бюджет на оплату отопления за месяц.
+
+Основная задача - это написать функцию isBudgetEnough,
+ которая буде возвращать строку. Если бюджета хватает для отопления 
+ всего объема торгового центра - выводится 'Бюджета достаточно', 
+ если нет - 'Бюджета недостаточно'. И все 🙂
+*/
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50255
+}
+
+function isBudgetEnough(data) {
+    let suma = 0
+    let volumTot =0
+    let contoTot= 0
+
+   data.shops.forEach(oo =>  //"oo"- e un parametro di function  
+        suma += oo.width * oo.length  //indcai chi viene interogato
+           )
+
+      volumTot = suma* data.height 
+      contoTot = data.budget-(volumTot * data.moneyPer1m3)
+
+      if(  contoTot>= 0){
+      //  console.log("Il badget basta per pagare tutto")
+      } else {
+      //  console.log("Purtroppo NON TI BASTA IL BUDGET")
+      }
+
+    //   console.log(suma)
+    //   console.log(volumTot)
+    //   console.log(contoTot)
+
+    
+}
+
+
+(isBudgetEnough(shoppingMallData))
+
+
+//------------------------------------------------------------------------------
+/*  2
+Задача:
+У вас есть список учеников, которые хотят поиграть в игру:
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 
+                    'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+Но команд может быть только 3 по 3 человека. 
+Напишите функцию sortStudentsByGroups, которая принимает в себя массив строк.
+Внутри она сначала сортирует имена по алфавиту. 
+Затем распределяет учеников по 3 человека в 3 группы по алфавитному порядку.
+Эти группы должны быть массивами. Как итог, функция возвращает новый массив
+с тремя командами и строкой как 4й элемент.
+
+Пример:
+sortStudentsByGroups(students)  =>
+
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: Takesi'
+]
+Если убрать одно студента из списка, то результат будет:
+
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: -'
+]
+А если добавить одного, то:
+
+[
+  [ 'Andrew', 'Ann', 'Bernard' ],
+  [ 'Cris', 'Josh', 'Mark' ],
+  [ 'Peter', 'Sam', 'Sandra' ],
+  'Оставшиеся студенты: Takesi, Somebody'
+]
+  */
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {  // dal 0 al 2
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}   //parentesi quadri del return hanno creato un unico array dove sara tutti gruppi dentro
+
+ console.log(sortStudentsByGroups(students));
